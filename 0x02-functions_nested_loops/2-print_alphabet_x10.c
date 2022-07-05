259963f2ede1a7feb2357461a687d44bdee7c9e4
julietking <ohagwajuliet6@gmail.com>
2022-07-05 16:29:04 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print the alphabet 10X
+* return - no need
+**/
+void print_alphabet_x10(void)
+{
+
+	int a, e;
+
+	a = 0;
+	while (a < 10)
+		{
+	e = 'a';
+	while (e <= 'z')
+		{
+	_putchar(e);
+	e++;
+		}
+	_putchar('\n');
+	a++;
+		}
+}
+
+Footer
+
+© 2022 GitHub, Inc.
