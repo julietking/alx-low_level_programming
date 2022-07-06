4e02a295620faf3e08a4a6a94a8bebbd5b8a6396
julietking <ohagwajuliet6@gmail.com>
2022-07-05 17:30:23 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _abs - absolute value of int
+* @n: int value
+* Return: int
+**/
+
+int _abs(int n)
+
+{
+	if (n < 0)
+	{
+	return (n * (-1));
+	}
+	else
+	{
+	return (n);
+	}
+}
