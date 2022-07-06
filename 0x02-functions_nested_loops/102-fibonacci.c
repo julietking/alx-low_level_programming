d18c507e541c23e5567c40abab951ef7ac3bbcba
julietking <ohagwajuliet6@gmail.com>
2022-07-06 08:13:58 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - program that prints the first 50 fib numbers
+* Return: always 0.
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned long int a = 0, b = 1, res = 0;
+
+	while (i < 50)
+	{
+		res = a + b;
+		a = b;
+		b = res;
+		prinif("%lu", res);
+		printf(", ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
