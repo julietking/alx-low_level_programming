2ae9c5b6ac4154f63f25f41abc5889af71fb4f26
julietking <ohagwajuliet6@gmail.com>
2022-07-07 20:24:58 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,11 +14,11 @@ void print_sqaure(int size)
 	{
 		for (x = 0; x < size; x++)
 		{
-			for (x = 0; y < size; y++)
+			for (y = 0; y < size; y++)
 			{
 			_putchar(35);
 			}
-		_putchar('\n')
+		_putchar('\n');
 		}
 		
 	}
