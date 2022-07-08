cd9697c2714f330bf0ca7421e3055b5f26479c5f
julietking <ohagwajuliet6@gmail.com>
2022-07-07 20:20:09 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square -> printing line
+ * @size: integer params
+ */
+void print_sqaure(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (x = 0; x < size; x++)
+			{
+			_putchar(35);
+			}
+		_putchar('\n')
+		}
+		
+	}
+}
