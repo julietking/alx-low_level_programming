4a37ee8450be0b6de609f1ab95fce3ef5d0f4248
julietking <ohagwajuliet6@gmail.com>
2022-07-12 14:38:55 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,6 +4,7 @@
  * print_rev -> printing a string in reverse
  * @s: the string to be printed in rev
  */
+
 void print_rev(char *s)
 {
 	int i, n;
@@ -11,9 +12,10 @@ void print_rev(char *s)
 	n = 0;
 	while (s[n] != '\0')
 		n++;
+
 	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
