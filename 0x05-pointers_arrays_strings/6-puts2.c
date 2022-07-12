3f6f10952af05a59fd9f15baf37d4d90c14b56be
julietking <ohagwajuliet6@gmail.com>
2022-07-12 14:54:49 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 -> puts2 function
+ * @str: str parameter
+ */
+void puts2(char *str)
+{
+	int len, i;
+
+	en = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
